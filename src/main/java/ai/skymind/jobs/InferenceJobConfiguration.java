eec52f0764ee41817dc8318354d5813d37b79bdd
Max Pumperla <max.pumperla@googlemail.com>
2019-02-11 17:06:02 +0100

    inference job config

--- src/main/java/ai/skymind/jobs/InferenceJobConfiguration.java
+++ src/main/java/ai/skymind/jobs/InferenceJobConfiguration.java
@@ -1,4 +1,35 @@
 package ai.skymind.jobs;
 
-public class InferenceJobConfiguration implements JobConfiguration {
+import ai.skymind.models.Model;
+import ai.skymind.resources.compute.ComputeResource;
+import ai.skymind.resources.storage.StorageResource;
+
+/**InferenceJobConfiguration
+ *
+ * Configuration for a SKIL inference job. On top of what you need to specify for a base JobConfiguration,
+ * you need to set the batch size for the model as well.
+ *
+ * @author Max Pumperla
+ */
+public class InferenceJobConfiguration extends JobConfiguration {
+
+    private int batchSize;
+
+
+    /**
+     * @param model Skil Model to run the job with
+     * @param batchSize Batch size used for model inference
+     * @param computeResource Compute resource
+     * @param storageResource Storage resource
+     * @param outputPath Path in which to store output data
+     * @param dataSetProviderClass Name of the `DataSetProvider` class used
+     * @param isMultiDataSet if input data is a MultiDataSet or not
+     * @param verbose log level, set true for detailed logging
+     */
+    public InferenceJobConfiguration(Model model, int batchSize, ComputeResource computeResource, StorageResource storageResource,
+                                     String outputPath, String dataSetProviderClass, boolean isMultiDataSet,
+                                     boolean verbose) {
+        super(model, computeResource, storageResource, outputPath, dataSetProviderClass, isMultiDataSet, verbose);
+        this.batchSize = batchSize;
+    }
 }
