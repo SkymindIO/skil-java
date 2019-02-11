b3814aa0a9d07dca9e87ecf27d098b7097c5c732
Max Pumperla <max.pumperla@googlemail.com>
2019-02-11 14:25:46 +0100

    hd insight

---
+++ src/main/java/ai/skymind/resources/compute/HDInsight.java
@@ -0,0 +1,61 @@
+package ai.skymind.resources.compute;
+
+import ai.skymind.ApiException;
+import ai.skymind.Skil;
+import ai.skymind.resources.Resource;
+import ai.skymind.skil.model.AddResourceRequest;
+import ai.skymind.skil.model.HDInsightResourceDetails;
+
+import java.util.Map;
+
+/** HDInsight
+ *
+ * Azure HDInsight compute resource.
+ *
+ * @author Max Pumperla
+ */
+public class HDInsight extends Resource {
+
+    private String name;
+    private String subscriptionId;
+    private String resourceGroupName;
+    private String clusterName;
+    private String credentialUri;
+
+    /**
+     *
+     * @param skil Skil instance
+     * @param name resource name
+     * @param subscriptionId Azure subscription ID
+     * @param resourceGroupName Azure resource group name
+     * @param clusterName HDInsight cluster name
+     * @param credentialUri path to credential file
+     */
+    public HDInsight(Skil skil, String name, String subscriptionId, String resourceGroupName,
+                     String clusterName, String credentialUri) throws ApiException {
+        super(skil);
+        this.name = name;
+        this.subscriptionId = subscriptionId;
+        this.resourceGroupName = resourceGroupName;
+        this.clusterName = clusterName;
+        this.credentialUri = credentialUri;
+
+        HDInsightResourceDetails details = new HDInsightResourceDetails()
+                .clusterName(this.clusterName)
+                .subscriptionId(this.subscriptionId)
+                .resourceGroupName(this.resourceGroupName);
+
+        AddResourceRequest request = new AddResourceRequest()
+                .resourceName(this.name)
+                .credentialUri(this.credentialUri)
+                .resourceDetails(details.toString())
+                .type(AddResourceRequest.TypeEnum.COMPUTE)
+                .subType(AddResourceRequest.SubTypeEnum.DATAPROC);
+
+        Object response = this.skil.getApi().addResource(request);
+
+        // TODO test if this casting works
+        this.resourceId = (Long) ((Map<String, Object>) response).get("resourceId");
+    }
+    }
+}
