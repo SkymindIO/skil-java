030694081836eacd21f30113628f050d318e3ff2
Max Pumperla <max.pumperla@googlemail.com>
2019-02-27 12:32:10 +0100

    fix model test

--- src/test/java/ai/skymind/models/ModelTest.java
+++ src/test/java/ai/skymind/models/ModelTest.java
@@ -3,11 +3,16 @@ package ai.skymind.models;
 import ai.skymind.Experiment;
 import ai.skymind.Skil;
 import ai.skymind.WorkSpace;
+import com.google.common.collect.Maps;
 import org.junit.Test;
 import org.nd4j.linalg.io.ClassPathResource;
 
 
 import java.io.File;
+import java.util.Map;
+import java.util.UUID;
+
+import static junit.framework.TestCase.assertTrue;
 
 public class ModelTest {
 
@@ -31,7 +36,27 @@ public class ModelTest {
         Experiment experiment = new Experiment(workSpace);
 
         File modelFile = new ClassPathResource("keras_mnist.h5").getFile();
-        Model model = new Model(modelFile, experiment, "Amnesiac", "Morning Bell", "v1",
+        Model model = new Model(modelFile, experiment, "Amnesiac" + UUID.randomUUID().toString(),
+                "Morning Bell", "v1",
                 "Packt Like Sardines in a Crushd Tin Box", false);
+
+        model.addEvaluation(0.98, "Hunting Bears" + UUID.randomUUID().toString(),
+                "I might be wrong", 1);
+
+        Map config = model.getConfig();
+        assertTrue(config.get("modelId").equals(model.getId()));
+        assertTrue(config.get("modelName").equals(model.getName()));
+        assertTrue(config.get("experimentId").equals(model.getExperiment().getId()));
+        assertTrue(config.get("workspaceId").equals(model.getExperiment().getWorkSpace().getId()));
+
+        String temp = File.createTempFile("model", ".json").getAbsolutePath();
+        model.save(temp);
+
+        Model recov = Model.load(skil, temp);
+
+        Map recovConfig = recov.getConfig();
+        assertTrue(Maps.difference(recovConfig, config).areEqual());
+
+//        model.delete();
     }
 }
